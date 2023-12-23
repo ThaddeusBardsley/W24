ac27f00fed2ebd927f49cf0ebc0aba6f2afb9932
OliverB11 <56227725+Brickwolves@users.noreply.github.com>
2023-12-23 12:44:58 -0500

    Added baseOpMode and stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleOpModes/IterativeTeleOp.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode.ExampleOpModes;
+
+import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
+import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+//@Disabled
+@TeleOp(name="Example Iterative TeleOp", group="Iterative Opmode")
+public class IterativeTeleOp extends OpMode {
+
+    // Declare OpMode members.
+    private final ElapsedTime runtime = new ElapsedTime();
+
+
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        setOpMode(this);
+
+        /*
+                    Y O U R   C O D E   H E R E
+                                                    */
+
+        multTelemetry.addData("Status", "Initialized");
+        multTelemetry.addLine(":-)");
+        multTelemetry.update();
+    }
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+
+        /*
+                    Y O U R   C O D E   H E R E
+                                                    */
+
+        multTelemetry.addData("Status", "InitLoop");
+        multTelemetry.update();
+    }
+
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+
+
+        /*
+                    Y O U R   C O D E   H E R E
+                                                   */
+
+        multTelemetry.addData("Status", "Started");
+        multTelemetry.update();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+
+    @Override
+    public void loop() {
+
+    /*
+         ----------- L O G G I N G -----------
+                                            */
+        multTelemetry.addData("Status", "Loop Active");
+        multTelemetry.update();
+    }
+
+
+    /*
+     * Code to run ONCE when the driver hits STOP
+     */
+    @Override
+    public void stop(){
+
+        /*
+                    Y O U R   C O D E   H E R E
+                                                   */
+
+    }
+}
