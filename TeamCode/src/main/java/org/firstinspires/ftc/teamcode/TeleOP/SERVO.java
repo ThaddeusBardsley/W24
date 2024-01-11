4999d58b50131365a23c3d8998d8460951679353
ThaddeusBardsley <thaddeusbardsley2@gmail.com>
2024-01-11 18:47:15 -0500

    ggd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/SERVO.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode.TeleOP;
+
+
+import static org.firstinspires.ftc.teamcode.Utilities.DashPos.servo1pos;
+import static org.firstinspires.ftc.teamcode.Utilities.DashPos.servo2pos;
+import static org.firstinspires.ftc.teamcode.Utilities.DashPos.servo3pos;
+import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
+import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+//@Disabled
+@TeleOp(name="TeleOp servo", group="Iterative Opmode")
+public class SERVO extends OpMode {
+    Servo bob;
+    Servo jamie;
+    Servo casie;
+
+
+
+
+    /*
+    Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        setOpMode(this);
+        bob = hardwareMap.get(Servo.class,"bob");
+        jamie = hardwareMap.get(Servo.class, "jamie");
+        casie = hardwareMap.get(Servo.class, "casie");
+
+        multTelemetry.addData("Status", "Initialized");
+        multTelemetry.addLine(":-)");
+        multTelemetry.update();
+    }
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+
+        /*
+                    Y O U R   C O D E   H E R E
+                                                    */
+
+        multTelemetry.addData("Status", "InitLoop");
+        multTelemetry.update();
+    }
+
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+
+        /*
+                    Y O U R   C O D E   H E R E
+                                                   */
+
+        multTelemetry.addData("Status", "Started");
+        multTelemetry.update();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+
+    @Override
+    public void loop() {
+        //This changes the speed);
+        bob.setPosition(servo3pos);
+        jamie.setPosition(servo2pos);
+        casie.setPosition(servo1pos);
+
+        multTelemetry.addData("Status", "Loop Active");
+        multTelemetry.update();
+
+    }
+
+
+    /*
+     * Code to run ONCE when the driver hits STOP
+     */
+    @Override
+    public void stop(){
+
+        /*
+                    Y O U R   C O D E   H E R E
+                                                   */
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/DashPos.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class DashPos {
+    public static double servo3pos = 0;
+    public static double servo1pos = 0;
+    public static double servo2pos = 0;
+
+}
